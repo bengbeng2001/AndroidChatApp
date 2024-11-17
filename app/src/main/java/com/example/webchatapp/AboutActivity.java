f68cbfa1732bc21b07593522bef2a74e410fc009
bengbeng2001 <annasnuur30@gmail.com>
2024-11-17 22:14:28 +0700

    adding login, homepage, profile, about, help

---
+++ app/src/main/java/com/example/webchatapp/AboutActivity.java
@@ -0,0 +1,4 @@
+package com.example.webchatapp;
+
+public class AboutActivity {
+}
--- app/src/main/java/com/example/webchatapp/MainActivity.java
+++ app/src/main/java/com/example/webchatapp/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.webchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnlogin;
+
+    private String KEY_NAME = "NAMA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inisialisasi Views
+        btnlogin = findViewById(R.id.btnLogin);
+        username = findViewById(R.id.usernameLogin);
+        password = findViewById(R.id.passwordLogin);
+
+        // Action untuk Button Login
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameLogin = username.getText().toString();
+                String passwordLogin = password.getText().toString();
+
+                if (!usernameLogin.isEmpty() && usernameLogin.equals("Annas")
+                        && (!passwordLogin.isEmpty() && passwordLogin.equals("1234"))) {
+                    Intent i = new Intent(MainActivity.this, HomepageActivity.class);
+                    i.putExtra(KEY_NAME, usernameLogin); // Mengirimkan nama pengguna
+                    startActivity(i);
+                } else {
+                    Toast.makeText(MainActivity.this, "Username / Password Wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
